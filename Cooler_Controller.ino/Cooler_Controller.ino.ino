e93e71aed9db5be8cbfd678cdcef8db8455e068d
Tatu P <tatu.polvinen@helsinki.fi>
2021-07-01 18:25:04 +0300

    Added rotary encoder switch/push button

--- Cooler_Controller.ino/Cooler_Controller.ino.ino
+++ Cooler_Controller.ino/Cooler_Controller.ino.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Adafruit_SleepyDog.h>
 #include <SPI.h>
 #include <Controllino.h>
@@ -10,11 +11,17 @@
 #include <avr/wdt.h>
 #include <LiquidCrystal_I2C.h>
 
+
+int8_t encoderSwitchPin = CONTROLLINO_D2;
+int8_t encoderSwitchState = HIGH;
+uint32_t encoderSwitchLowMs;
+uint32_t showEepromStoreSign;
+
 Encoder myEnc(CONTROLLINO_IN0, CONTROLLINO_IN1);
 int32_t encoderValue = 0;
 int32_t oldEncoderValue = -999;
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const int16_t sensorReadingIntervalSeconds = 10;
 const int16_t numberOfSensorReadingsPerMeasurement = 10;
@@ -39,15 +46,15 @@ void error(char *str)
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
-
+  pinMode(encoderSwitchPin, INPUT_PULLUP);
   Wire.begin();
 
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(3,0);
+  lcd.setCursor(3, 0);
   lcd.print("Hello, world!");
 
-  int countdownMS = Watchdog.enable(4000);
+  int16_t countdownMS = Watchdog.enable(4000);
   Watchdog.reset();
 
   DPRINTLN();
@@ -86,11 +93,46 @@ void loop() {
   Watchdog.reset();
 
   encoderValue = myEnc.read();
-  if (encoderValue != oldEncoderValue) {
-    oldEncoderValue = encoderValue;
-  }
+  //  if (encoderValue != oldEncoderValue) {
+  //    oldEncoderValue = encoderValue;
+  //  }
   Serial.println(encoderValue);
-  
+
+  int8_t prevEncoderSwitchState = encoderSwitchState;
+  encoderSwitchState = digitalRead(encoderSwitchPin);
+  if (prevEncoderSwitchState == HIGH && encoderSwitchState == LOW) {
+    encoderSwitchLowMs = millis();
+  } else if (prevEncoderSwitchState == LOW && encoderSwitchState == HIGH) {
+    if (millis() - encoderSwitchLowMs < 50) {
+      // ignoring debounce
+    }
+    else {
+      // do the right thing!
+
+      lcd.setCursor(3, 2);
+      lcd.print("STORED");
+      showEepromStoreSign = millis();
+    }
+  }
+
+  if (encoderSwitchState == HIGH) {
+    lcd.setCursor(3, 3);
+    lcd.print("    ");
+    lcd.setCursor(3, 3);
+    lcd.print("HIGH");
+  } else {
+    lcd.setCursor(3, 3);
+    lcd.print("    ");
+    lcd.setCursor(3, 3);
+    lcd.print("LOW");
+  }
+
+  if (millis() - showEepromStoreSign > 5000) {
+    lcd.setCursor(3, 2);
+      lcd.print("      ");
+  }
+
+
   int16_t sensorReadings[numberOfSensorReadingsPerMeasurement];
   const unsigned long sensorReadingIntervalMilliseconds = sensorReadingIntervalSeconds * 1000;
 
