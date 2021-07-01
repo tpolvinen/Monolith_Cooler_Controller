e2ae83153c05fdbdc806f244da823c5ae2d01581
Tatu P <tatu.polvinen@helsinki.fi>
2021-07-01 21:27:36 +0300

    Added set temperature, basic valve & fan control

--- Cooler_Controller.ino/Cooler_Controller.ino.ino
+++ Cooler_Controller.ino/Cooler_Controller.ino.ino
@@ -11,6 +11,10 @@
 #include <avr/wdt.h>
 #include <LiquidCrystal_I2C.h>
 
+int16_t eepromAddr = 0;
+int16_t setTemp = 0;
+float temperature1;
+float temperature2;
 
 int8_t encoderSwitchPin = CONTROLLINO_D2;
 int8_t encoderSwitchState = HIGH;
@@ -19,15 +23,18 @@ uint32_t showEepromStoreSign;
 
 Encoder myEnc(CONTROLLINO_IN0, CONTROLLINO_IN1);
 int32_t encoderValue = 0;
-int32_t oldEncoderValue = -999;
+//int32_t oldEncoderValue = -999;
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const int16_t sensorReadingIntervalSeconds = 10;
 const int16_t numberOfSensorReadingsPerMeasurement = 10;
 
-#define ONE_WIRE_BUS0   CONTROLLINO_D0
-#define ONE_WIRE_BUS1   CONTROLLINO_D1
+#define ONE_WIRE_BUS0 CONTROLLINO_D0
+#define ONE_WIRE_BUS1 CONTROLLINO_D1
+#define COOLANT_FLOW_VALVE CONTROLLINO_R0
+#define FAN_RELAY CONTROLLINO_R1
+
 OneWire oneWire_one(ONE_WIRE_BUS0);
 OneWire oneWire_two(ONE_WIRE_BUS1);
 DallasTemperature sensor_one(&oneWire_one);
@@ -40,6 +47,9 @@ void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
   pinMode(encoderSwitchPin, INPUT_PULLUP);
+  pinMode(COOLANT_FLOW_VALVE, OUTPUT);
+  pinMode(FAN_RELAY, OUTPUT);
+
   Wire.begin();
 
   lcd.init();
@@ -50,6 +60,20 @@ void setup() {
   int16_t countdownMS = Watchdog.enable(4000);
   Watchdog.reset();
 
+  setTemp = EEPROM.read(eepromAddr);
+
+  if (setTemp > 40) {
+    setTemp -= 256;
+  }
+
+  lcd.setCursor(3, 0);
+  lcd.print("             ");
+  lcd.setCursor(3, 0);
+  lcd.print("Set Temp: ");
+  lcd.print(setTemp);
+  lcd.setCursor(3, 2);
+  lcd.print("Dial: ");
+
   DPRINTLN();
   DPRINTLN("------------------------------------------------------------");
   DPRINTLN("              Cooler Controller starting!");
@@ -57,10 +81,13 @@ void setup() {
   DPRINT("     Enabled the watchdog with max countdown of ");
   DPRINT(countdownMS, DEC);
   DPRINTLN(" ms");
+  DPRINTLN();
+  DPRINT("Set Temp: ");
+  DPRINTLN(setTemp);
   DPRINTLN("------------------------------------------------------------");
   DPRINTLN();
-  DPRINT("Reading rotary encoder, value: ");
-  DPRINTLN(encoderValue = myEnc.read());
+  //  DPRINT("Reading rotary encoder, value: ");
+  //  DPRINTLN(encoderValue = myEnc.read());
 
   float temperature;
   sensor_one.begin();
@@ -69,14 +96,21 @@ void setup() {
 
   sensor_one.setResolution(resolution);
   sensor_one.requestTemperatures();
+  lcd.setCursor(0, 1);
+  lcd.print("T1: ");
+  temperature1 = sensor_one.getTempCByIndex(0);
+  lcd.print(temperature1);
   DPRINT("Initializing sensor_one... temperature: ");
-  DPRINT(sensor_one.getTempCByIndex(0));
+  DPRINT(temperature1);
   DPRINTLN(" ...Done.");
 
   sensor_two.setResolution(resolution);
   sensor_two.requestTemperatures();
+  lcd.print(" T2: ");
+  temperature2 = sensor_two.getTempCByIndex(0);
+  lcd.print(temperature1);
   DPRINT("Initializing sensor_two... temperature: ");
-  DPRINT(sensor_two.getTempCByIndex(0));
+  DPRINT(temperature2);
   DPRINTLN(" ...Done.");
 
 }
@@ -86,10 +120,31 @@ void loop() {
   Watchdog.reset();
 
   encoderValue = myEnc.read();
-  //  if (encoderValue != oldEncoderValue) {
-  //    oldEncoderValue = encoderValue;
-  //  }
   Serial.println(encoderValue);
+
+  lcd.setCursor(9, 2);
+  lcd.print("       ");
+  lcd.setCursor(9, 2);
+  lcd.print(encoderValue);
+
+  sensor_one.requestTemperatures();
+  lcd.setCursor(4, 1);
+  temperature1 = sensor_one.getTempCByIndex(0);
+  lcd.print(temperature1);
+  sensor_two.requestTemperatures();
+  lcd.setCursor(14, 1);
+  temperature2 = sensor_two.getTempCByIndex(0);
+  lcd.print(temperature2);
+
+
+  if (temperature1 > setTemp) {
+    digitalWrite(COOLANT_FLOW_VALVE, HIGH);
+    digitalWrite(FAN_RELAY, HIGH);
+  } else {
+    digitalWrite(COOLANT_FLOW_VALVE, LOW);
+    digitalWrite(FAN_RELAY, LOW);
+  }
+
 
   int8_t prevEncoderSwitchState = encoderSwitchState;
   encoderSwitchState = digitalRead(encoderSwitchPin);
@@ -100,38 +155,38 @@ void loop() {
       // ignoring debounce
     }
     else {
-      // do the right thing!
-
-      lcd.setCursor(3, 2);
-      lcd.print("STORED");
+      if (encoderValue < -20) {
+        setTemp = -20;
+      } else if (encoderValue > 40) {
+        setTemp = 40;
+      } else {
+        setTemp = encoderValue;
+      }
+      EEPROM.write(eepromAddr, setTemp);
+      lcd.setCursor(0, 3);
+      lcd.print("Set Temp: ");
+      lcd.print(setTemp);
       showEepromStoreSign = millis();
     }
   }
 
-  if (encoderSwitchState == HIGH) {
-    lcd.setCursor(3, 3);
-    lcd.print("    ");
-    lcd.setCursor(3, 3);
-    lcd.print("HIGH");
-  } else {
-    lcd.setCursor(3, 3);
-    lcd.print("    ");
-    lcd.setCursor(3, 3);
-    lcd.print("LOW");
-  }
-
   if (millis() - showEepromStoreSign > 5000) {
-    lcd.setCursor(3, 2);
-      lcd.print("      ");
+    lcd.setCursor(0, 3);
+    lcd.print("                    ");
+    lcd.setCursor(0, 0);
+    lcd.print("                    ");
+    lcd.setCursor(3, 0);
+    lcd.print("Set Temp: ");
+    lcd.print(setTemp);
   }
 
 
-  int16_t sensorReadings[numberOfSensorReadingsPerMeasurement];
-  const unsigned long sensorReadingIntervalMilliseconds = sensorReadingIntervalSeconds * 1000;
-
-  for (int8_t i = 0; i < numberOfSensorReadingsPerMeasurement; i++) {
-    // int16_t sensorReading =
-  }
+  //  int16_t sensorReadings[numberOfSensorReadingsPerMeasurement];
+  //  const unsigned long sensorReadingIntervalMilliseconds = sensorReadingIntervalSeconds * 1000;
+  //
+  //  for (int8_t i = 0; i < numberOfSensorReadingsPerMeasurement; i++) {
+  //
+  //  }
 
 
 
